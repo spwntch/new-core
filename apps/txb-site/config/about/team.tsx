202b79f829e8430fcbeb297e1cfaa89dc931681f
zpydee <benji@spawntech.io>
2024-09-08 14:53:53 +0200

    adds team config

---
+++ apps/txb-site/config/about/team.tsx
@@ -0,0 +1,124 @@
+import { IContent } from '@spwntch/react-ui';
+
+export const TEAM: IContent = {
+  heading: {
+    content: "Meet Our Team",
+    className: "text-4xl font-bold  text-center",
+  },
+  subheading: {
+    content: "We’re a dynamic group of individuals who are passionate about what we do.",
+    className: "text-lg text-gray-300 text-center mb-6",
+  },
+  bullets: [
+    {
+      image: { src: "/images/team/dave-benjamin.webp", alt: "Dave Benjamin" }, // Replace with actual image paths
+      heading: "Dave Benjamin",
+      subheading: "Founder & CEO",
+      body: [],
+      bullets: [
+        // {
+        //   icon: <LinkedinIcon />,
+        //   href: "https://www.linkedin.com/in/dave-benjamin", 
+        // },
+      ],
+    },
+    {
+      image: { src: "/images/team/gary-greenfield.webp", alt: "Gary Greenfield" },
+      heading: "Gary Greenfield",
+      subheading: "Co-Founder & Managing Director",
+      body: [],
+    //   bullets: [
+    //     {
+    //       icon: <LinkedinIcon />,
+    //       href: "https://www.linkedin.com/in/gary-greenfield",
+    //     },
+    //   ],
+    },
+    {
+      image: { src: "/images/team/khensani-ngobeni.webp", alt: "Khensani Ngobeni" },
+      heading: "Khensani Ngobeni",
+      subheading: "Customer Success",
+      body: [],
+    //   bullets: [
+    //     {
+    //     //   icon: <LinkedinIcon />,
+    //     //   href: "https://www.linkedin.com/in/khensani-ngobeni-690662a5/",
+    //     },
+    //   ],
+    },
+    {
+      image: { src: "/images/team/kgomotsego-chiri.webp", alt: "Kgomotsego Chiri" },
+      heading: "Kgomotsego Chiri",
+      subheading: "Customer Success",
+      body: [],
+    //   bullets: [
+    //     {
+    //       icon: <LinkedinIcon />,
+    //       href: "https://www.linkedin.com/in/kgomotsego-chiri-8a31771a0/",
+    //     },
+    //   ],
+    },
+    {
+      image: { src: "/images/team/candice-malcolm.webp", alt: "Candice Malcolm" },
+      heading: "Candice Malcolm",
+      subheading: "Content Expert",
+      body: [],
+    //   bullets: [
+    //     {
+    //       icon: <LinkedinIcon />,
+    //       href: "https://www.linkedin.com/in/candice-malcolm-00b1b524/",
+    //     },
+    //   ],
+    },
+    {
+      image: { src: "/images/team/arshaad-yousuph.webp", alt: "Arshaad Yousuph" },
+      heading: "Arshaad Yousuph",
+      subheading: "CX Expert",
+      body: [],
+    //   bullets: [
+    //     {
+    //       icon: <LinkedinIcon />,
+    //       href: "https://www.linkedin.com/in/arshaadyousuph/",
+    //     },
+    //   ],
+    },
+    {
+      image: { src: "/images/team/tshitshi-ntumba.webp", alt: "Tshitshi Ntumba" },
+      heading: "Tshitshi Ntumba",
+      subheading: "UX Expert",
+      body: [],
+    //   bullets: [
+    //     {
+    //       icon: <LinkedinIcon />,
+    //       href: "https://www.linkedin.com/in/tshitshintumba/",
+    //     },
+    //   ],
+    },
+    {
+      image: { src: "/images/team/leanne-rimer.webp", alt: "Leanne Rimer" },
+      heading: "Leanne Rimer",
+      subheading: "EX Expert",
+      body: [],
+    //   bullets: [
+    //     {
+    //       icon: <LinkedinIcon />,
+    //       href: "https://www.linkedin.com/in/leannerimer/",
+    //     },
+    //   ],
+    },
+    {
+      image: { src: "/images/team/eddie-ukpong.webp", alt: "Eddie Ukpong" },
+      heading: "Eddie Ukpong",
+      subheading: "Design Thinking Expert",
+      body: [],
+    //   bullets: [
+    //     {
+    //       icon: <LinkedinIcon />,
+    //       href: "https://www.linkedin.com/in/eddie-ukpong-652691b8/",
+    //     },
+    //   ],
+    },
+  ],
+};
+
+export default meetOurTeamSection;
