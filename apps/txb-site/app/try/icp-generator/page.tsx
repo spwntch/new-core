a89a3301ef5b41ece432b3583ec03a49bf195e0b
zpydee <benji@spawntech.io>
2024-09-09 17:02:23 +0200

    starting on step 1

--- apps/txb-site/app/try/icp-generator/page.tsx
+++ apps/txb-site/app/try/icp-generator/page.tsx
@@ -1,9 +1,28 @@
 import { TryProductStepper } from '@/next-marketing-pages';
 
 const TryIcpGenerator = () => {
+  const steps = [
+    {
+      title: 'Product Resarch',
+      description: `Let's get started by doing some product research. The more information you have about your product, the better.`,
+    },
+    {
+      title: 'Generate ICP',
+      description: 'Step 2 description',
+    },
+    {
+      title: 'Generate Buyer Personas',
+      description: 'Step 3 description',
+    },
+    {
+      title: 'Review Target Customers',
+      description: 'Step 3 description',
+    },
+   
+  ];
   return (
     <div>
-        <TryProductStepper />
+      <TryProductStepper steps={steps} />
     </div>
   );
 };
--- libs/next-marketing-pages/src/lib/client-components/page-sections/try-product-stepper/try-product-stepper.tsx
+++ libs/next-marketing-pages/src/lib/client-components/page-sections/try-product-stepper/try-product-stepper.tsx
@@ -1,14 +1,21 @@
 'use client';
 
 import { Stepper, StepperButtons } from '@spwntch/react-ui';
+import { useState } from 'react';
 
-export const TryProductStepper = () => {
+type TryProductStepperProps = {
+  steps: { title: string; description: string }[];
+};
+
+export const TryProductStepper = ({ steps }: TryProductStepperProps) => {
+  const [currentStep, setCurrentStep] = useState(0);
   return (
     <div className="pt-24 px-8">
       <div className="container w-full mx-auto shadow-xl rounded-2xl pb-2">
         <div className="horizontal mt-5">
-          <Stepper />
+          <Stepper steps={steps} currentStep={currentStep} />
         </div>
+        {<div className='py-4'>{steps?.[currentStep]?.description}</div>}
         <StepperButtons />
       </div>
     </div>
