931b86a020edcb1c46f9411c61042ddff78bad96
zpydee <benji@spawntech.io>
2024-09-09 15:53:34 +0200

    better layout

--- apps/txb-site/app/try/icp-generator/page.tsx
+++ apps/txb-site/app/try/icp-generator/page.tsx
@@ -1,7 +1,12 @@
+import IcpGenerator from '../../../components/icp-generator';
 import React from 'react';
 
 const TryIcpGenerator = () => {
-  return <div className='pt-32'>TryIcpGenerator</div>;
+  return (
+    <div className="pt-32">
+      <IcpGenerator />
+    </div>
+  );
 };
 
 export default TryIcpGenerator;
