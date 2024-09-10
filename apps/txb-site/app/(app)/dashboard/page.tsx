66cab175b2df536f723cdf5ad32c4eb2029a5253
zpydee <benji@spawntech.io>
2024-09-10 12:37:36 +0200

    adds a spearate dahbaord example

--- apps/txb-site/app/(app)/dashboard/page.tsx
+++ apps/txb-site/app/(app)/dashboard/page.tsx
@@ -1,8 +1,8 @@
-import { Dashboard } from '../../../components/dashboard/dashboard';
+import { Dashboard1 } from '../../../components/dashboards/dashboard1';
 import React from 'react';
 
 const Page = () => {
-  return <Dashboard />;
+  return <Dashboard1 />;
 };
 
 export default Page;
--- apps/txb-site/components/dashboards/dashboard1.tsx
+++ apps/txb-site/components/dashboards/dashboard1.tsx
@@ -55,7 +55,7 @@ import {
 export const description =
   'A products dashboard with a sidebar navigation and a main content area. The dashboard has a header with a search input and a user menu. The sidebar has a logo, navigation links, and a card with a call to action. The main content area shows an empty state with a call to action.';
 
-export function Dashboard() {
+export function Dashboard1() {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
