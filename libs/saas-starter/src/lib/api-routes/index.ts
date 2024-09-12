cbea0e8ab0503fac98be87ebe0025d39f3a4c252
zpydee <benji@spawntech.io>
2024-09-12 22:16:58 +0200

    adds auth routes to saas-starter

---
+++ libs/saas-starter/src/lib/api-routes/index.ts
@@ -0,0 +1,2 @@
+export {GET as getAuthCallback} from './auth-callback/route'
+export {GET as getAuthResetPassword} from './auth-reset-password/route'
--- libs/saas-starter/src/server.ts
+++ libs/saas-starter/src/server.ts
@@ -1,3 +1,4 @@
 // Use this file to export React server components
 export * from './lib/utils/supabase/server';
 export * from './lib/utils/supabase/middleware';
+export * from './lib/api-routes';
