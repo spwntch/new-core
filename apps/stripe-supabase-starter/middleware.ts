1dabe9c2a3612bdeffe0fff97da0ab9a42301767
zpydee <benji@spawntech.io>
2024-09-13 11:02:19 +0200

    deletes now unused utils and types from app

--- apps/stripe-supabase-starter/middleware.ts
+++ apps/stripe-supabase-starter/middleware.ts
@@ -1,5 +1,5 @@
 import { type NextRequest } from 'next/server';
-import { updateSession } from './utils/supabase/middleware';
+import { updateSession } from '@spwntch/saas-starter/server';
 
 export async function middleware(request: NextRequest) {
   return await updateSession(request);
