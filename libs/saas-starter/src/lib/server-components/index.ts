18c28f052990c969f5d2a48a9fe1d4046c01a275
zpydee <benji@spawntech.io>
2024-09-13 10:08:42 +0200

    moves account page to saas-starter

--- libs/saas-starter/src/lib/server-components/index.ts
+++ libs/saas-starter/src/lib/server-components/index.ts
@@ -1,2 +1,3 @@
+export { default as AccountPage } from './account-page/account-page';
 export { default as SignInPage } from './sign-in-page/sign-in-page';
 export { default as SignInPaths } from './sign-in-paths/sign-in-paths';
