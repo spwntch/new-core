a651651b9f351bd8e72338e2b1c85a081a7b9b5b
zpydee <benji@spawntech.io>
2024-09-13 09:17:24 +0200

    adds convenience export of stripe webhooks

--- libs/saas-starter/src/lib/api-routes/index.ts
+++ libs/saas-starter/src/lib/api-routes/index.ts
@@ -1,2 +1,4 @@
-export {GET as getAuthCallback} from './auth-callback/route'
-export {GET as getAuthResetPassword} from './auth-reset-password/route'
+export { GET as getAuthCallback } from './auth-callback/route';
+export { GET as getAuthResetPassword } from './auth-reset-password/route';
+export { POST as postStripeWebhooks } from './stripe-webhooks/route';
+
